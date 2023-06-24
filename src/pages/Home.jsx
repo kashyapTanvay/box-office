bf02b3086ad7d151233a4db233d0b11785222fbe
Tanvay Kashyap <kashyaptanvay@gmail.com>
2023-06-25 02:09:59 +0530

    added adiDataError, renderApi, used try and catch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+import { searchForShows } from '../api/tvmaze';
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
+
+  const onSearchInputChnage = ev => {
+    setSearchStr(ev.target.value);
+  };
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    try {
+      setApiDataError(null);
+
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error Occured: {apiDataError.message}</div>;
+    }
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChnage} />
+        <button type="submit">Search</button>
+      </form>
+      <div>{renderApiData()}</div>
+    </div>
+  );
 };
 
 export default Home;
