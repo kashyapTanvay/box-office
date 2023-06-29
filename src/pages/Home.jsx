420bcadcd06369e043c04487ef06b5b8111caacb
Tanvay Kashyap <kashyaptanvay@gmail.com>
2023-06-29 12:21:39 +0530

    input and button added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const [inputValue, setInputValue] = useState('');
+  console.log(inputValue);
+
+  const onInputChange = ev => {
+    setInputValue(ev.target.value);
+  };
+
+  return (
+    <div>
+      <div>{inputValue}</div>
+      <input type="text" value={inputValue} onChange={onInputChange} />
+      <button
+        type="button"
+        onClick={() => {
+          setInputValue('Tanvay');
+        }}
+      >
+        Update to Random
+      </button>
+    </div>
+  );
 };
 
 export default Home;
