d2890f3a95345d5ea927c131da329d267a2d1069
Tanvay Kashyap <kashyaptanvay@gmail.com>
2023-06-30 21:52:16 +0530

    added searchforshows and prettier fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,46 @@
 import { useState } from 'react';
+import { searchForShows } from './../api/tvMaze';
 
 const Home = () => {
-  const [inputValue, setInputValue] = useState('');
-  console.log(inputValue);
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
-  const onInputChange = ev => {
-    setInputValue(ev.target.value);
+  const onSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div> Error Occured : {apiDataError.message}</div>;
+    }
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
   };
 
   return (
     <div>
-      <div>{inputValue}</div>
-      <input type="text" value={inputValue} onChange={onInputChange} />
-      <button
-        type="button"
-        onClick={() => {
-          setInputValue('Tanvay');
-        }}
-      >
-        Update to Random
-      </button>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+
+      <div>{renderApiData()}</div>
     </div>
   );
 };
